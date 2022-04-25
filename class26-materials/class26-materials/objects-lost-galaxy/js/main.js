4eb58224c143aa9557cf26675af4b5f8a060c0c2
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-25 09:53:59 +0200

    objects-lost-galaxy solved

--- class26-materials/class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,22 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.size = 'standard'
+pizza.toppings = ['cheese', 'peperoni', 'jalapenos']
+pizza.shape = 'rectangle'
+pizza.cheese = true
+
+pizza.cut = function() {
+    console.log('8 nice slices for you')
+}
+
+pizza.heat = function(){
+    console.log('Its hawt')
+}
+
+pizza.whatToppings = function(){
+    pizza.toppings.forEach(top => {
+        console.log(top)
+    })
+}
