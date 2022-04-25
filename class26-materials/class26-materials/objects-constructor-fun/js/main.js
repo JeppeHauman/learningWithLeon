ea67a461ca11eed76054b2da0b099314cc797c97
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-25 10:28:36 +0200

    objects-constructor-fun solved

--- class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+
+function MakePizza(size, toppings, crust, sauce) {
+    this.size = size
+    this.toppings = toppings 
+    this.crust = crust
+    this.sauce = sauce
+
+    this.burnMouth = function() {
+        console.log('Aauuii aauuiii')
+    }
+
+    this.cut = function() {
+        console.log('slizzeeee')
+    }
+
+    this.toppings = function() {
+        console.log(this.toppings)
+    }
+}
