456a7d4140e3f693807d2a1e6a0da0a959e059d4
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-26 09:00:18 +0200

    Got fetch working on nasa api

--- class26-materials/class26-materials/objects-nasa/js/main.js
+++ class26-materials/class26-materials/objects-nasa/js/main.js
@@ -1,1 +1,12 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+const apiKey = '?api_key=RkvuirMn6V1N8Jq5Ksm4oL4OEixzl4ou4lzOdFcL'
+function getAPOD(){
+    fetch('https://api.nasa.gov/planetary/apod' + apiKey)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('img').src = data.hdurl
+        })
+}
+
+getAPOD()
