a3622de0e87953b43f4cc63cfb1acfb6eaee9484
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-27 09:32:54 +0200

    objects-netflix-shows assignment. First class with a constructor and methods

--- class27-materials/class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,19 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
-
+class MakeNetflixTVShow {
+    constructor(title, seasons, castNum, rating){
+        this.title = title;
+        this.seasons = seasons;
+        this.castNum = castNum;
+        this.rating = rating;
+    }
+    play(){
+        console.log('Now playing..')
+    }
+    recap(){
+        console.log("Here's a quick recap")
+    }
+    startOver(){
+        console.log('Starting from season 1, episode 1')
+    }
+}
