99ad04beabfd8b35f76a094012a0314c749a49cb
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-27 10:45:01 +0200

    First localStorage stuff

--- class27-materials/class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,13 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
+document.querySelector('button').addEventListener('click', goUpOne)
+if(!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore', 0)
+}
 
+
+function goUpOne(){
+    let botScoreVal = Number(localStorage.getItem('botScore'))
+    botScoreVal += 1
+    localStorage.setItem('botScore', botScoreVal)
+}
