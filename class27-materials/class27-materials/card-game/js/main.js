dddb28bc2afb54fde30cfd976a58d77fcf0ee000
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-28 14:25:01 +0200

    Start of lesson 28

--- class27-materials/class27-materials/card-game/js/main.js
+++ class27-materials/class27-materials/card-game/js/main.js
@@ -1,14 +1,30 @@
 //Cardgame called WAR
-document.querySelector('button').addEventListener('click', getFetch)
+if(!localStorage.getItem('deckId'))
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    localStorage.setItem('deckId', data.deck_id)
+  })
 
+document.querySelector('.newDeck').addEventListener('click', getNewDeck)
+function getNewDeck() {
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem('deckId', data.deck_id)
+    })
+}
+
+document.querySelector('.draw').addEventListener('click', getFetch)
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
-  fetch(url)
+  fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckId')}/draw/?count=2`)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('.player1Card').src = data.cards[0].image
+        document.querySelector('.player2Card').src = data.cards[1].image
+        
       })
       .catch(err => {
           console.log(`error ${err}`)
