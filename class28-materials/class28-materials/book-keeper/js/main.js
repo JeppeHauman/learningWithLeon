0160b85e2bb0f4d2492fba662dd74e0fdac04cf4
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-04-29 17:02:44 +0200

    Books are getting into a string, and stored in localStorage

--- class28-materials/class28-materials/book-keeper/js/main.js
+++ class28-materials/class28-materials/book-keeper/js/main.js
@@ -5,21 +5,24 @@ document.querySelector('h2').innerText = localStorage.getItem('books')
 function getFetch(){
   const isbn = document.querySelector('input').value
   const url = `https://openlibrary.org/isbn/${isbn}.json`
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data.title)
-        if(localStorage.getItem('books')){
-          let books = localStorage.getItem('books') + " ; " + data.title
-          localStorage.setItem('books', books)       
-        } else {
-          localStorage.setItem('books', data.title)
-        }
-        document.querySelector('h2').innerText = localStorage.getItem('books')
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+  if(isbn){
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data.title)
+          if(localStorage.getItem('books')){
+            let books = localStorage.getItem('books') + " ; " + data.title
+            localStorage.setItem('books', books)       
+          } else {
+            localStorage.setItem('books', data.title)
+          }
+          document.querySelector('h2').innerText = localStorage.getItem('books')
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+      } else {
+        alert("Theres no booooook")
+      }
 }
 
