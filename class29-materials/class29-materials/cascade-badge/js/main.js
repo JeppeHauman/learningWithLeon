6543945303aaf4a2cb5656da833496c28048878d
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-01 00:09:33 +0200

    Start the cascade assignment

--- class29-materials/class29-materials/cascade-badge/js/main.js
+++ class29-materials/class29-materials/cascade-badge/js/main.js
@@ -1,11 +1,20 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+function reversedList(order) {
+    return order.reverse()
+}
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+function sumOfSquares(a, b){
+    return a.reduce((acc,c) => acc + c**2, 0) > b.reduce((acc,c) => acc + c**3, 0) 
+}
+
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
+let arr = []
+let newArr = arr.filter(num => num % arr.indexOf(num))
 
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
