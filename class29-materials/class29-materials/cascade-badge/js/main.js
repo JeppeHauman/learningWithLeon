ede3c5fb6b1717018da274fc9dea2c8f54ab436b
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-01 08:57:25 +0200

    make filter method work

--- class29-materials/class29-materials/cascade-badge/js/main.js
+++ class29-materials/class29-materials/cascade-badge/js/main.js
@@ -14,7 +14,8 @@ function sumOfSquares(a, b){
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
-let arr = []
-let newArr = arr.filter(num => num % arr.indexOf(num))
+function isMulti(arr) {
+    return arr.filter(num, i => num % i === 0)
+}
 
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
