0ca4326f62fc61d36b5820636d8e51778555fc9b
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-03 08:48:01 +0200

    espresso machine assignment

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,22 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine{
+    constructor(make, model, color, weight){
+        this.make = make
+        this.model = model
+        this.color = color 
+        this.weight = weight
+    }
+
+    makeEspresso(){
+        console.log('Brrrrrr')
+    }
+
+    grind(){
+        console.log('Bbbrr different')
+    }
+
+    clean(){
+        console.log('Also goes bbbrrr')
+    }
+}
