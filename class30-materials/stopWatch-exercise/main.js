ac373483627fa59077a19c3f63f1bee73352afb6
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-05 21:35:19 +0200

    started stopwatch exercise from homework video

---
+++ class30-materials/stopWatch-exercise/main.js
@@ -0,0 +1,12 @@
+class StopWatch {
+    constructor(){
+        let startTime, endTime, running, duration
+    }
+
+    start() {
+        if(!running) {
+            running = true
+            
+        }
+    }
+}
