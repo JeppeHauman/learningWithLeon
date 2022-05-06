af735ce8c7c84ce17db19e9c2dc2df5445636d5f
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-06 10:55:18 +0200

    First exercise with class extension

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,29 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal {
+    constructor(name){
+        this.name = name
+    }
+
+    speak() {
+        console.log(`${this.name} is SPEAKING!?`)
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, catBreed){
+        super(name)
+        this.catBreed = catBreed
+    }
+}
+
+class Cow extends Animal {
+    constructor(name, weight) {
+        super(name)
+        this.weight = weight
+    }
+}
+
+let muuuh = new Cow('Muuuh', 600)
+let choko = new Cat('Choko', 'Indoor thing')
+
