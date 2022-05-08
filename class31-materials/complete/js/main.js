b0b329f1b6f9dc360f757adb5d34d00096a389c8
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-08 17:10:44 +0200

    Played around with class and extend class

--- class31-materials/complete/js/main.js
+++ class31-materials/complete/js/main.js
@@ -6,42 +6,81 @@ class Animal{
         return this._name
     }
     speak(){
-        console.log(`${this._name} makes a sound`)
+        console.log(`${this._name} casually says hi`)
     }
 }
-class Dog extends Animal{
-    constructor(name,breed){
+class Domesticated extends Animal{
+    constructor(name, breed){
         super(name)
         this._breed = breed
     }
     get breed(){
         return this._breed
     }
-    speak(){
-        super.speak()
-        console.log(`${this.name} barks`)
-    }    
-}
-class Cat extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
 
-let farm = [simba,machi,salem]
 
-for( a of farm ){
-    a.speak()
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     speak(){
+//         console.log(`${this._name} makes a sound`)
+//     }
+// }
+// class Dog extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} barks`)
+//     }
+// }
+// class Cat extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} meows`)
+//     }
+// }
+
+// let simba = new Dog('Simba','Shepard')
+// let machi = new Dog('The Machine','Pitbull')
+// let salem = new Cat('Salem', 'American Shorthair')
+
+// let farm = [simba,machi,salem]
+
+// for( a of farm ){
+//     a.speak()
+// }
