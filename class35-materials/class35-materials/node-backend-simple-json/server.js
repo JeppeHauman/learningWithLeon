dc708d7f0746f349ccb2141da52fa5c17ee14741
Jeppe Hauman <jeppe_hauman@hotmail.com>
2022-05-25 09:57:45 +0200

    added figlet to run the backend

--- class35-materials/class35-materials/node-backend-simple-json/server.js
+++ class35-materials/class35-materials/node-backend-simple-json/server.js
@@ -9,30 +9,30 @@ const server = http.createServer((req, res) => {
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
   if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
   }
   else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('otherpage.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
   }
   else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('otherotherpage.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
   }
   else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+    if ('student' in params) {
+      if (params['student'] == 'leon') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const objToJson = {
           name: "leon",
           status: "Boss Man",
@@ -40,8 +40,8 @@ const server = http.createServer((req, res) => {
         }
         res.end(JSON.stringify(objToJson));
       }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+      else if (params['student'] != 'leon') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const objToJson = {
           name: "unknown",
           status: "unknown",
@@ -51,23 +51,23 @@ const server = http.createServer((req, res) => {
       }//student != leon
     }//student if
   }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
+  else if (page == '/css/style.css') {
+    fs.readFile('css/style.css', function (err, data) {
       res.write(data);
       res.end();
     });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
+  } else if (page == '/js/main.js') {
+    fs.readFile('js/main.js', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
       res.write(data);
       res.end();
     });
-  }else{
-    figlet('404!!', function(err, data) {
+  } else {
+    figlet('404!!', function (err, data) {
       if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
       }
       res.write(data);
       res.end();
